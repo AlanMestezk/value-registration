edc5635c4e0c1a5b1a41f5d845b00acfac615037
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-06-17 08:51:03 -0300

    praticando c# (Csharp)

---
+++ Aula90/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$");
+Console.WriteLine("CAIXA DE SUPERMERCADO!");
+Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$");
+Console.WriteLine(" ");
+Console.Write("Quantos produtos você quer somar?: ");
+
+int N = int.Parse(Console.ReadLine());
+int soma = 0;
+
+for (int i = 1; i <= N; i++) {
+    Console.Write("Valor do produto: #{0}: ", i);
+    int valor = int.Parse(Console.ReadLine());
+    soma += valor;
+}
+
+Console.WriteLine(" ");
+Console.WriteLine($"Valor total da compra: {soma} reais");
+
+
